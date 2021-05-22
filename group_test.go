6d014f8674d34f573b14c151e20c8c7ff3780c13
uberswe <markus@tenghamn.com>
2021-05-22 16:13:07 +0200

    removed duplicate lines for group tests

--- group_test.go
+++ group_test.go
@@ -27,23 +27,34 @@ func TestTournamentGroupGetTeams(t *testing.T) {
 			&team4,
 		},
 	}
-	if len(*tg.GetTeams()) != 4 {
-		t.Errorf("Number of teams on tournament group does not match %d != %d", len(*tg.GetTeams()), 4)
+	assertTeams(tg, t)
+}
+
+func TestTournamentGroupAppendTeam(t *testing.T) {
+	team1 := Team{ID: 1}
+	team2 := Team{ID: 2}
+	team3 := Team{ID: 3}
+	team4 := Team{ID: 4}
+	tg := Group{
+		ID: 0,
 	}
-	for i, team := range *tg.GetTeams() {
-		if i == 0 && team.GetID() != 1 {
-			t.Errorf("Team ID is not correct %d != %d", team.GetID(), 1)
-		}
-		if i == 1 && team.GetID() != 2 {
-			t.Errorf("Team ID is not correct %d != %d", team.GetID(), 2)
-		}
-		if i == 2 && team.GetID() != 3 {
-			t.Errorf("Team ID is not correct %d != %d", team.GetID(), 3)
-		}
-		if i == 3 && team.GetID() != 4 {
-			t.Errorf("Team ID is not correct %d != %d", team.GetID(), 4)
-		}
+	tg.AppendTeam(&team1)
+	tg.AppendTeam(&team2)
+	tg.AppendTeam(&team3)
+	tg.AppendTeam(&team4)
+	assertTeams(tg, t)
+}
+
+func TestTournamentGroupAppendTeams(t *testing.T) {
+	team1 := Team{ID: 1}
+	team2 := Team{ID: 2}
+	team3 := Team{ID: 3}
+	team4 := Team{ID: 4}
+	tg := Group{
+		ID: 0,
 	}
+	tg.AppendTeams([]TeamInterface{&team1, &team2, &team3, &team4})
+	assertTeams(tg, t)
 }
 
 func TestTournamentGroupGetGames(t *testing.T) {
@@ -60,23 +71,7 @@ func TestTournamentGroupGetGames(t *testing.T) {
 			&game4,
 		},
 	}
-	if len(*tg.GetGames()) != 4 {
-		t.Errorf("Number of games on tournament group does not match %d != %d", len(*tg.GetTeams()), 4)
-	}
-	for i, g := range *tg.GetGames() {
-		if i == 0 && g.GetID() != 1 {
-			t.Errorf("Game ID is not correct %d != %d", g.GetID(), 1)
-		}
-		if i == 1 && g.GetID() != 2 {
-			t.Errorf("Game ID is not correct %d != %d", g.GetID(), 2)
-		}
-		if i == 2 && g.GetID() != 3 {
-			t.Errorf("Game ID is not correct %d != %d", g.GetID(), 3)
-		}
-		if i == 3 && g.GetID() != 4 {
-			t.Errorf("Game ID is not correct %d != %d", g.GetID(), 4)
-		}
-	}
+	assertGames(tg, t)
 }
 
 func TestTournamentGroupAppendGame(t *testing.T) {
@@ -91,23 +86,7 @@ func TestTournamentGroupAppendGame(t *testing.T) {
 	tg.AppendGame(&game2)
 	tg.AppendGame(&game3)
 	tg.AppendGame(&game4)
-	if len(*tg.GetGames()) != 4 {
-		t.Errorf("Number of games on tournament group does not match %d != %d", len(*tg.GetTeams()), 4)
-	}
-	for i, g := range *tg.GetGames() {
-		if i == 0 && g.GetID() != 1 {
-			t.Errorf("Game ID is not correct %d != %d", g.GetID(), 1)
-		}
-		if i == 1 && g.GetID() != 2 {
-			t.Errorf("Game ID is not correct %d != %d", g.GetID(), 2)
-		}
-		if i == 2 && g.GetID() != 3 {
-			t.Errorf("Game ID is not correct %d != %d", g.GetID(), 3)
-		}
-		if i == 3 && g.GetID() != 4 {
-			t.Errorf("Game ID is not correct %d != %d", g.GetID(), 4)
-		}
-	}
+	assertGames(tg, t)
 }
 
 func TestTournamentGroupAppendGames(t *testing.T) {
@@ -119,82 +98,7 @@ func TestTournamentGroupAppendGames(t *testing.T) {
 		ID: 0,
 	}
 	tg.AppendGames([]GameInterface{&game1, &game2, &game3, &game4})
-	if len(*tg.GetGames()) != 4 {
-		t.Errorf("Number of games on tournament group does not match %d != %d", len(*tg.GetTeams()), 4)
-	}
-	for i, g := range *tg.GetGames() {
-		if i == 0 && g.GetID() != 1 {
-			t.Errorf("Game ID is not correct %d != %d", g.GetID(), 1)
-		}
-		if i == 1 && g.GetID() != 2 {
-			t.Errorf("Game ID is not correct %d != %d", g.GetID(), 2)
-		}
-		if i == 2 && g.GetID() != 3 {
-			t.Errorf("Game ID is not correct %d != %d", g.GetID(), 3)
-		}
-		if i == 3 && g.GetID() != 4 {
-			t.Errorf("Game ID is not correct %d != %d", g.GetID(), 4)
-		}
-	}
-}
-
-func TestTournamentGroupAppendTeam(t *testing.T) {
-	team1 := Team{ID: 1}
-	team2 := Team{ID: 2}
-	team3 := Team{ID: 3}
-	team4 := Team{ID: 4}
-	tg := Group{
-		ID: 0,
-	}
-	tg.AppendTeam(&team1)
-	tg.AppendTeam(&team2)
-	tg.AppendTeam(&team3)
-	tg.AppendTeam(&team4)
-	if len(*tg.GetTeams()) != 4 {
-		t.Errorf("Number of teams on tournament group does not match %d != %d", len(*tg.GetTeams()), 4)
-	}
-	for i, team := range *tg.GetTeams() {
-		if i == 0 && team.GetID() != 1 {
-			t.Errorf("Team ID is not correct %d != %d", team.GetID(), 1)
-		}
-		if i == 1 && team.GetID() != 2 {
-			t.Errorf("Team ID is not correct %d != %d", team.GetID(), 2)
-		}
-		if i == 2 && team.GetID() != 3 {
-			t.Errorf("Team ID is not correct %d != %d", team.GetID(), 3)
-		}
-		if i == 3 && team.GetID() != 4 {
-			t.Errorf("Team ID is not correct %d != %d", team.GetID(), 4)
-		}
-	}
-}
-
-func TestTournamentGroupAppendTeams(t *testing.T) {
-	team1 := Team{ID: 1}
-	team2 := Team{ID: 2}
-	team3 := Team{ID: 3}
-	team4 := Team{ID: 4}
-	tg := Group{
-		ID: 0,
-	}
-	tg.AppendTeams([]TeamInterface{&team1, &team2, &team3, &team4})
-	if len(*tg.GetTeams()) != 4 {
-		t.Errorf("Number of teams on tournament group does not match %d != %d", len(*tg.GetTeams()), 4)
-	}
-	for i, team := range *tg.GetTeams() {
-		if i == 0 && team.GetID() != 1 {
-			t.Errorf("Team ID is not correct %d != %d", team.GetID(), 1)
-		}
-		if i == 1 && team.GetID() != 2 {
-			t.Errorf("Team ID is not correct %d != %d", team.GetID(), 2)
-		}
-		if i == 2 && team.GetID() != 3 {
-			t.Errorf("Team ID is not correct %d != %d", team.GetID(), 3)
-		}
-		if i == 3 && team.GetID() != 4 {
-			t.Errorf("Team ID is not correct %d != %d", team.GetID(), 4)
-		}
-	}
+	assertGames(tg, t)
 }
 
 func TestTournamentGroupPrint(t *testing.T) {
@@ -223,5 +127,27 @@ func TestTournamentGroupPrint(t *testing.T) {
 	}
 	if tg.Print() != "Group ID: 0\nTeam ID: 1\nTeam ID: 2\nTeam ID: 3\nTeam ID: 4\n\n" {
 		t.Errorf("Tournament group print does not match %s != %s", tg.Print(), "Group ID: 0\nTeam ID: 1\nTeam ID: 2\nTeam ID: 3\nTeam ID: 4\n\n")
+	}
+}
+
+func assertGames(tg Group, t *testing.T) {
+	if len(*tg.GetGames()) != 4 {
+		t.Errorf("Number of games on tournament group does not match %d != %d", len(*tg.GetGames()), 4)
+	}
+	for i, game := range *tg.GetGames() {
+		if game.GetID() != i+1 {
+			t.Errorf("Game ID is not correct %d != %d", game.GetID(), 1)
+		}
+	}
+}
+
+func assertTeams(tg Group, t *testing.T) {
+	if len(*tg.GetTeams()) != 4 {
+		t.Errorf("Number of teams on tournament group does not match %d != %d", len(*tg.GetTeams()), 4)
+	}
+	for i, team := range *tg.GetTeams() {
+		if team.GetID() != i+1 {
+			t.Errorf("Team ID is not correct %d != %d", team.GetID(), 1)
+		}
 	}
 }
