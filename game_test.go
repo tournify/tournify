f6486b02da10fc90f904111f083afdd24f3b578a
uberswe <markus@tenghamn.com>
2021-05-25 21:36:10 +0200

    Added more tests for game functions

--- game_test.go
+++ game_test.go
@@ -13,6 +13,17 @@ func TestGameGetID(t *testing.T) {
 	}
 }
 
+func TestGameGetParentIDs(t *testing.T) {
+	s := []int{2, 3}
+	game := Game{
+		ID:        1,
+		ParentIDs: s,
+	}
+	if !isEqual(game.GetParentIDs(), s) {
+		t.Errorf("ParentIDs are not equal %v: %v\n", game.GetParentIDs(), s)
+	}
+}
+
 func TestGameSetScore(t *testing.T) {
 	homeScore := 10.0
 	awayScore := 5.0
@@ -30,11 +41,26 @@ func TestGameSetScore(t *testing.T) {
 	if game.GetAwayScore().GetPoints() != awayScore {
 		t.Errorf("AwayScore does not match %.2f != %.2f\n", game.GetAwayScore().GetPoints(), awayScore)
 	}
+
+	score := Score{Points: 10}
+	teams2 := []TeamInterface{&homeTeam, &awayTeam}
+	game2 := Game{
+		ID:     1,
+		Teams:  teams2,
+		Scores: []ScoreInterface{&score},
+	}
+	game2.SetScore(homeScore, awayScore)
+	if game2.GetHomeScore().GetPoints() != homeScore {
+		t.Errorf("HomeScore does not match %.2f != %.2f\n", game.GetHomeScore().GetPoints(), homeScore)
+	}
+	if game2.GetAwayScore().GetPoints() != awayScore {
+		t.Errorf("AwayScore does not match %.2f != %.2f\n", game.GetAwayScore().GetPoints(), awayScore)
+	}
 }
 
 func TestGameGetHomeAwayTeams(t *testing.T) {
-	homeTeam := Team{ID: 0}
-	awayTeam := Team{ID: 1}
+	homeTeam := Team{ID: 1}
+	awayTeam := Team{ID: 2}
 	teams := []TeamInterface{&homeTeam, &awayTeam}
 	game := Game{
 		ID:    1,
@@ -45,6 +71,62 @@ func TestGameGetHomeAwayTeams(t *testing.T) {
 	}
 	if game.GetAwayTeam().GetID() != awayTeam.GetID() {
 		t.Errorf("Away team ids does not match %d != %d\n", game.GetAwayTeam().GetID(), awayTeam.GetID())
+	}
+
+	game2 := Game{
+		ID: 1,
+	}
+	if game2.GetHomeTeam().GetID() != 0 {
+		t.Errorf("Home team ids does not match %d != %d\n", game2.GetHomeTeam().GetID(), 0)
+	}
+	if game2.GetAwayTeam().GetID() != 0 {
+		t.Errorf("Away team ids does not match %d != %d\n", game2.GetAwayTeam().GetID(), 0)
+	}
+}
+
+func TestGameSetHomeTeam(t *testing.T) {
+	homeTeam := Team{ID: 1}
+	game := Game{
+		ID: 1,
+	}
+
+	game.SetHomeTeam(&homeTeam)
+	if game.GetHomeTeam().GetID() != homeTeam.GetID() {
+		t.Errorf("Home team ids does not match %d != %d\n", game.GetHomeTeam().GetID(), homeTeam.GetID())
+	}
+
+	homeTeam2 := Team{ID: 99}
+	game.SetHomeTeam(&homeTeam2)
+	if game.GetHomeTeam().GetID() != homeTeam2.GetID() {
+		t.Errorf("Home team ids does not match %d != %d\n", game.GetHomeTeam().GetID(), homeTeam2.GetID())
+	}
+}
+
+func TestGameSetAwayTeam(t *testing.T) {
+	awayTeam := Team{ID: 1}
+	game := Game{
+		ID: 1,
+	}
+
+	game.SetAwayTeam(&awayTeam)
+	if game.GetAwayTeam().GetID() != awayTeam.GetID() {
+		t.Errorf("Away team ids does not match %d != %d\n", game.GetAwayTeam().GetID(), awayTeam.GetID())
+	}
+
+	if game.GetHomeTeam().GetID() != 0 {
+		t.Errorf("Home team ids does not match %d != %d\n", game.GetHomeTeam().GetID(), 0)
+	}
+
+	homeTeam := Team{ID: 22}
+	game.SetHomeTeam(&homeTeam)
+
+	awayTeam2 := Team{ID: 99}
+	game.SetAwayTeam(&awayTeam2)
+	if game.GetAwayTeam().GetID() != awayTeam2.GetID() {
+		t.Errorf("Away team ids does not match %d != %d\n", game.GetAwayTeam().GetID(), awayTeam2.GetID())
+	}
+	if game.GetHomeTeam().GetID() != homeTeam.GetID() {
+		t.Errorf("Home team ids does not match %d != %d\n", game.GetHomeTeam().GetID(), homeTeam.GetID())
 	}
 }
 
